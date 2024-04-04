5a73f909bfd93e67b39268cc0656ac5eea165128
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-04-03 21:31:04 -0500

    custom styles for text

--- components/paragraph_text.go
+++ components/paragraph_text.go
@@ -1,12 +1,25 @@
 package components
 
+import "strings"
+
 type Text struct {
 	alignment string
 	text      string
+	styles    []string
 }
 
 func NewText(text string) Text {
 	return Text{text: text}
+}
+
+func (t Text) Style(property string, value string) Text {
+	style := property + ": " + value + ";"
+	t.styles = append(t.styles, style)
+	return t
+}
+
+func (t Text) joinStyles() string {
+	return strings.Join(t.styles, " ")
 }
 
 func (t Text) Centered() Text {
@@ -15,8 +28,11 @@ func (t Text) Centered() Text {
 }
 
 func (t Text) Align(alignment string) Text {
-	t.alignment = alignment
-	return t
+	return t.Style("text-align", alignment)
+}
+
+func (t Text) Size(size string) Text {
+	return t.Style("font-size", size)
 }
 
 func (t Text) ParagraphPlain() string {
@@ -24,6 +40,6 @@ func (t Text) ParagraphPlain() string {
 }
 
 func (t Text) Paragraph() string {
-	alignment := orDefault(t.alignment, "left")
-	return `<div style="text-align: ` + alignment + `;">` + t.text + `</div>`
+	styles := t.joinStyles()
+	return `<div style="` + styles + `">` + t.text + `</div>`
 }
